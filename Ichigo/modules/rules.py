ce10aaacc305e52d8cbd56819be393b2acdbefc2
DARKCYBERGANG <71146813+DARKCYBERGANG@users.noreply.github.com>
2021-04-07 19:15:00 +0000

    Update rules.py

--- Ichigo/modules/rules.py
+++ Ichigo/modules/rules.py
@@ -1,9 +1,9 @@
 from typing import Optional
 
-import SaitamaRobot.modules.sql.rules_sql as sql
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.helper_funcs.string_handling import markdown_parser
+import Ichigo.modules.sql.rules_sql as sql
+from Ichigo import dispatcher
+from Ichigo.modules.helper_funcs.chat_status import user_admin
+from Ichigo.modules.helper_funcs.string_handling import markdown_parser
 from telegram import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
