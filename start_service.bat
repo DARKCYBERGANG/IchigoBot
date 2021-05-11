ef961886ead064bf9d992aad7fb074faf418ff24
DARKCYBERGANG <71146813+DARKCYBERGANG@users.noreply.github.com>
2021-05-11 12:44:49 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop Ichigo
-net start Ichigo
+net stop IchigoBot
+net start IchigoBot
