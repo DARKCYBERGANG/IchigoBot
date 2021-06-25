56d31d37df5ad414fa483d9988c95dd2acdb4b37
PEARLGANG <73771895+PEARLGANG@users.noreply.github.com>
2021-06-25 11:21:18 +0530

    Update user.py

--- Ichigo/user.py
+++ Ichigo/user.py
@@ -19,7 +19,7 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
-from config import Config
+from Ichigo.config import Config
 from pyrogram import Client
 
 USER = Client(
