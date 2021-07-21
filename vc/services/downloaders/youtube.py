5601655e3fac67307fc911dc7847b17c575f2ea9
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-07-21 23:25:33 +0530

    Create youtube.py

---
+++ vc/services/downloaders/youtube.py
@@ -0,0 +1,33 @@
+
+from os import path
+
+from youtube_dl import YoutubeDL
+
+from vc.config import DURATION_LIMIT
+from vc.helpers.errors import DurationLimitError
+
+ydl_opts = {
+    "format": "bestaudio[ext=m4a]",
+    "geo-bypass": True,
+    "nocheckcertificate": True,
+    "outtmpl": "downloads/%(id)s.%(ext)s",
+}
+
+ydl = YoutubeDL(ydl_opts)
+
+
+def download(url: str) -> str:
+    info = ydl.extract_info(url, False)
+    duration = round(info["duration"] / 60)
+
+    if duration > DURATION_LIMIT:
+        raise DurationLimitError(
+            f"**Sorry!** 😞 \n__I Can't Play Songs Longer Than {DURATION_LIMIT} Minutes!__",
+        )
+    try:
+        ydl.download([url])
+    except:
+        raise DurationLimitError(
+            f"**Sorry!** 😞 \n__I Can't Play Songs Longer Than {DURATION_LIMIT} Minutes!__",
+        )
+    return path.join("downloads", f"{info['id']}.{info['ext']}")
