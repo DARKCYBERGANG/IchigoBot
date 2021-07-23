43d0c77bd6cd1f3bf44745d5e3a8136d18a2299d
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-07-24 03:51:02 +0530

    Update event.py

--- Ichigo/event.py
+++ Ichigo/event.py
@@ -1,5 +1,9 @@
 from telethon import events
 from Ichigo import telethn
+import sys
+import traceback
+from functools import wraps
+from Ichigo import rnd, LOG_GROUP
 
 
 def register(**args):
@@ -59,3 +63,29 @@ def callbackquery(**args):
         return func
 
     return decorator
+
+def capture_err(func):
+    @wraps(func)
+    async def capture(client, message, *args, **kwargs):
+        try:
+            return await func(client, message, *args, **kwargs)
+        except Exception as err:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            errors = traceback.format_exception(
+                etype=exc_type, value=exc_obj, tb=exc_tb,
+            )
+            error_feedback = split_limits(
+                '**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n'.format(
+                    0 if not message.from_user else message.from_user.id,
+                    0 if not message.chat else message.chat.id,
+                    message.text or message.caption,
+                    ''.join(errors),
+                ),
+            )
+            for x in error_feedback:
+                await rnd.send_message(
+                    LOG_GROUP,
+                    x
+                )
+            raise err
+    return capture
