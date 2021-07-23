8718993a1ce34445eccea7e2ac0e75c4b5b6684f
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-07-23 23:20:43 +0530

    Create AI.py

---
+++ Ichigo/modules/AI.py
@@ -0,0 +1,170 @@
+import re
+from asyncio import gather, get_event_loop, sleep
+import html
+from time import time
+import Ichigo.modules.sql.chatbot_sql as sql
+from aiohttp import ClientSession
+from pyrogram import Client, filters, idle
+from Python_ARQ import ARQ
+from Ichigo import ARQ_API_BASE_URL, ARQ_API_KEY, LANGUAGE, TOKEN, dispatcher
+from Ichigo.modules.helper_funcs.chat_status import user_admin
+from Ichigo.modules.helper_funcs.filters import CustomFilters
+from Ichigo.modules.log_channel import gloggable
+from telegram import Update
+from telegram.error import BadRequest, RetryAfter, Unauthorized
+from telegram.ext import (
+    CallbackContext,
+    CommandHandler,
+    Filters,
+    MessageHandler,
+    run_async,
+)
+from telegram.utils.helpers import mention_html
+
+luna = Client(
+    ":memory:",
+    bot_token=TOKEN,
+    api_id=6,
+    api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e",
+)
+
+bot_id = int(bot_token.split(":")[0])
+arq = None
+
+
+async def lunaQuery(query: str, user_id: int):
+    query = (
+        query
+        if LANGUAGE == "en"
+        else (await arq.translate(query, "en")).result.translatedText
+    )
+    resp = (await arq.luna(query, user_id)).result
+    return (
+        resp
+        if LANGUAGE == "en"
+        else (
+            await arq.translate(resp, LANGUAGE)
+        ).result.translatedText
+    )
+
+
+async def type_and_send(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id if message.from_user else 0
+    query = message.text.strip()
+    await message._client.send_chat_action(chat_id, "typing")
+    response, _ = await gather(rndQuery(query, user_id), sleep(1))
+    await message.reply_text(response)
+    await message._client.send_chat_action(chat_id, "cancel")
+
+
+
+
+@luna.on_message(filters.command("help") & ~filters.edited)
+async def start(_, message):
+    await luna.send_chat_action(message.chat.id, "typing")
+    await sleep(1)
+    await message.reply_text("KeK")
+
+
+@luna.on_message(
+    ~filters.private
+    & filters.text
+    & ~filters.command("help")
+    & ~filters.edited,
+    group=69,
+)
+async def chat(_, message):
+    if message.reply_to_message:
+        if not message.reply_to_message.from_user:
+            return
+        from_user_id = message.reply_to_message.from_user.id
+        if from_user_id != bot_id:
+            return
+    else:
+        match = re.search(
+            "[.|\n]{0,}rnd[.|\n]{0,}",
+            message.text.strip(),
+            flags=re.IGNORECASE,
+        )
+        if not match:
+            return
+    await type_and_send(message)
+
+
+@luna.on_message(
+    filters.private & ~filters.command("help") & ~filters.edited
+)
+async def chatpm(_, message):
+    if not message.text:
+        return
+    await type_and_send(message)
+
+
+    session = ClientSession()
+    arq = ARQ(ARQ_API_BASE_URL, ARQ_API_KEY, session)
+
+@run_async
+@user_admin
+@gloggable
+def add_chat(update: Update, context: CallbackContext):
+    global arq
+    chat = update.effective_chat
+    msg = update.effective_message
+    user = update.effective_user
+    is_chat = sql.is_chat(chat.id)
+    if chat.type == "private":
+        msg.reply_text("You can't enable AI in PM.")
+        return
+
+    if not is_chat:
+        ses = arq.create_session()
+        ses_id = str(ses.id)
+        expires = str(ses.expires)
+        sql.set_ses(chat.id, ses_id, expires)
+        msg.reply_text("AI successfully enabled for this chat!")
+        message = (
+            f"<b>{html.escape(chat.title)}:</b>\n"
+            f"#AI_ENABLED\n"
+            f"<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
+        )
+        return message
+    else:
+        msg.reply_text("AI is already enabled for this chat!")
+        return ""
+
+
+@run_async
+@user_admin
+@gloggable
+def remove_chat(update: Update, context: CallbackContext):
+    msg = update.effective_message
+    chat = update.effective_chat
+    user = update.effective_user
+    is_chat = sql.is_chat(chat.id)
+    if not is_chat:
+        msg.reply_text("AI isn't enabled here in the first place!")
+        return ""
+    else:
+        sql.rem_chat(chat.id)
+        msg.reply_text("AI disabled successfully!")
+        message = (
+            f"<b>{html.escape(chat.title)}:</b>\n"
+            f"#AI_DISABLED\n"
+            f"<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
+        )
+        return message
+
+     
+    
+    
+ADD_CHAT_HANDLER = CommandHandler("addchat", add_chat)
+REMOVE_CHAT_HANDLER = CommandHandler("rmchat", remove_chat)
+
+dispatcher.add_handler(ADD_CHAT_HANDLER)
+dispatcher.add_handler(REMOVE_CHAT_HANDLER)
+
+__handlers__ = [
+    ADD_CHAT_HANDLER,
+    REMOVE_CHAT_HANDLER,
+]
