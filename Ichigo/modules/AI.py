a9a2816e00ec07aabbd2e4567e284b52f8210d8f
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-07-24 00:35:34 +0530

    Update AI.py

--- Ichigo/modules/AI.py
+++ Ichigo/modules/AI.py
@@ -5,7 +5,7 @@ from aiohttp import ClientSession
 from pyrogram import Client, filters, idle
 from Python_ARQ import ARQ
 
-from config import ARQ_API_BASE_URL, ARQ_API_KEY, LANGUAGE, TOKEN
+from Ichigo import ARQ_API_BASE_URL, ARQ_API_KEY, LANGUAGE, TOKEN
 
 luna = Client(
     ":memory:",
