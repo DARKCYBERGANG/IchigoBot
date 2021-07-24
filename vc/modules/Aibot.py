48b210385095ae34167d8a5ae5421bb45f74a773
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-07-24 17:02:49 +0530

    Create Aibot.py

---
+++ vc/modules/Aibot.py
@@ -0,0 +1,88 @@
+from pyrogram import Client, filters
+from vc.config import arq
+
+    
+
+__MODULE__ = "ChatBot"
+__HELP__ = "/chatbot [ON|OFF] To Enable Or Disable ChatBot In Your Chat."
+
+active_chats = []
+
+# Enabled | Disable Chatbot
+BOT_ID = 1690374847
+chatbot_group = 69
+
+def capture_err(func):
+    @wraps(func)
+    async def capture(client, message, *args, **kwargs):
+        try:
+            return await func(client, message, *args, **kwargs)
+        except Exception as err:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            errors = traceback.format_exception(
+                etype=exc_type, value=exc_obj, tb=exc_tb,
+            )
+            error_feedback = split_limits(
+                '**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n'.format(
+                    0 if not message.from_user else message.from_user.id,
+                    0 if not message.chat else message.chat.id,
+                    message.text or message.caption,
+                    ''.join(errors),
+                ),
+            )
+            for x in error_feedback:
+                await Client.send_message(
+                    LOG_GROUP,
+                    x
+                )
+            raise err
+    return capture
+
+
+
+@Client.on_message(filters.command("chatbot") & ~filters.edited)
+@capture_err
+async def chatbot_status(_, message):
+    global active_chats
+    if len(message.command) != 2:
+        await message.reply_text("/chatbot [ON|OFF]")
+        return
+    status = message.text.split(None, 1)[1]
+    chat_id = message.chat.id
+
+    if status == "ON" or status == "on" or status == "On":
+        if chat_id not in active_chats:
+            active_chats.append(chat_id)
+            text = "Chatbot Enabled Reply To Any Message" \
+                   + "Of Mine To Get A Reply"
+            await message.reply_text(text)
+            return
+        await message.reply_text("ChatBot Is Already Enabled.")
+        return
+
+    elif status == "OFF" or status == "off" or status == "Off":
+        if chat_id in active_chats:
+            active_chats.remove(chat_id)
+            await message.reply_text("Chatbot Disabled!")
+            return
+        await message.reply_text("ChatBot Is Already Disabled.")
+        return
+
+    else:
+        await message.reply_text("/chatbot [ON|OFF]")
+
+
+@Client.on_message(filters.text & filters.reply & ~filters.bot &
+                ~filters.via_bot & ~filters.forwarded, group=chatbot_group)
+@capture_err
+async def chatbot_talk(_, message):
+    if message.chat.id not in active_chats:
+        return
+    if message.reply_to_message.from_user.id != BOT_ID:
+        return
+    query = message.text
+    luna = await arq.luna(query)
+    response = luna.response
+    await message.reply_text(response)
+    
+    
