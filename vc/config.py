c0046a3e20174075ed2455ce2cb9bb7089056a08
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-07-24 21:53:27 +0530

    Update config.py

--- vc/config.py
+++ vc/config.py
@@ -8,6 +8,7 @@ if os.path.exists("local.env"):
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 TOKEN = getenv("TOKEN")
+BOT_ID = int(TOKEN.split(":")[0])
 BOT_NAME = getenv("BOT_NAME")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "AsmSafone")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/5fed93ca29be49d1a6ed0.png")
