93e4f74aedce8c0ce728c856b0a372244f9c7897
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-07-24 22:16:00 +0530

    Create botinfo.py

---
+++ vc/botinfo.py
@@ -0,0 +1,13 @@
+from wbb import LOG_GRP
+from pyrogram import Client
+BOT_ID = 0
+BOT_NAME = ""
+
+async def get_info(Client):
+    global BOT_ID, BOT_NAME
+    getme = await Client.get_me()
+    BOT_ID = getme.id
+    try:
+        await Client.send_message(LOG_GRP, text=f"{BOT_NAME} Started!")
+    except:
+        pass
