f3511ae83a4f25d49606788506e6b74bb9c5e675
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-07-24 19:19:32 +0530

    Update Aibot.py

--- vc/modules/Aibot.py
+++ vc/modules/Aibot.py
@@ -3,19 +3,8 @@ from vc.config import ARQ_API_BASE_URL, ARQ_API_KEY, LOG_GRP
 from aiohttp import ClientSession
 from Python_ARQ import ARQ
 from functools import wraps
-import sys
-import traceback
-
-import aiohttp
-async def fetch(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            try:
-                data = await resp.json()
-            except:
-                data = await resp.text()
-    return data
-
+from vc.fetch import fetch
+from vc.errors import capture_err
 
 session = ClientSession()
 arq = ARQ(ARQ_API_BASE_URL, ARQ_API_KEY, session)
@@ -29,52 +18,6 @@ active_chats = []
 # Enabled | Disable Chatbot
 BOT_ID = 1690374847
 chatbot_group = 69
-
-def split_limits(text):
-    if len(text) < 2048:
-        return [text]
-
-    lines = text.splitlines(True)
-    small_msg = ''
-    result = []
-    for line in lines:
-        if len(small_msg) + len(line) < 2048:
-            small_msg += line
-        else:
-            result.append(small_msg)
-            small_msg = line
-    else:
-        result.append(small_msg)
-
-    return result
-
-
-def capture_err(func):
-    @wraps(func)
-    async def capture(client, message, *args, **kwargs):
-        try:
-            return await func(client, message, *args, **kwargs)
-        except Exception as err:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            errors = traceback.format_exception(
-                etype=exc_type, value=exc_obj, tb=exc_tb,
-            )
-            error_feedback = split_limits(
-                '**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n'.format(
-                    0 if not message.from_user else message.from_user.id,
-                    0 if not message.chat else message.chat.id,
-                    message.text or message.caption,
-                    ''.join(errors),
-                ),
-            )
-            for x in error_feedback:
-                await Client.send_message(
-                    LOG_GRP,
-                    x
-                )
-            raise err
-    return capture
-
 
 
 @Client.on_message(filters.command("chatbot") & ~filters.edited)
