2f27ce59b6abc4c8d1fa2face209d0bb257a12e1
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-07-24 12:29:14 +0530

    Create config.py

---
+++ Ichigo/config.py
@@ -0,0 +1,8 @@
+bot_token = "1690374847:AAFIyej-OT4CEFDu8CaMiTekXa4sMJAnFlo"
+
+api_id = 6400129
+
+api_hash = "c1b151498dd24438519e3017097b46ab"
+
+# Leave This As It Is
+ARQ_API_BASE_URL = "https://thearq.tech"
