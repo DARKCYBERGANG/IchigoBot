a9d5878556cfaa9a801079809b345915fe10bd96
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-07-24 18:50:44 +0530

    Update Aibot.py

--- vc/modules/Aibot.py
+++ vc/modules/Aibot.py
@@ -19,6 +19,25 @@ active_chats = []
 BOT_ID = 1690374847
 chatbot_group = 69
 
+def split_limits(text):
+    if len(text) < 2048:
+        return [text]
+
+    lines = text.splitlines(True)
+    small_msg = ''
+    result = []
+    for line in lines:
+        if len(small_msg) + len(line) < 2048:
+            small_msg += line
+        else:
+            result.append(small_msg)
+            small_msg = line
+    else:
+        result.append(small_msg)
+
+    return result
+
+
 def capture_err(func):
     @wraps(func)
     async def capture(client, message, *args, **kwargs):
